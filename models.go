41a8bab418af30623b0ff66713372e988c33ed10
MikeB1124 <56797038+MikeB1124@users.noreply.github.com>
2024-06-24 13:35:28 -0700

    change pattern data entry field to interface

--- models.go
+++ models.go
@@ -15,17 +15,17 @@ type PatternWebhookRequest struct {
 }
 
 type PatternData struct {
-	PatternType   string  `json:"patterntype" bson:"patterntype"`
-	PatternName   string  `json:"patternname" bson:"patternname"`
-	ProfitOne     float64 `json:"profit1" bson:"profit1"`
-	DisplaySymbol string  `json:"displaysymbol" bson:"displaysymbol"`
-	Symbol        string  `json:"symbol" bson:"symbol"`
-	StopLoss      float64 `json:"stoploss" bson:"stoploss"`
-	PatternUrl    string  `json:"url" bson:"url"`
-	TimeFrame     string  `json:"timeframe" bson:"timeframe"`
-	Status        string  `json:"status" bson:"status"`
-	Entry         string  `json:"entry" bson:"entry"`
-	PatternClass  string  `json:"patternclass" bsom:"patternclass"`
+	PatternType   string      `json:"patterntype" bson:"patterntype"`
+	PatternName   string      `json:"patternname" bson:"patternname"`
+	ProfitOne     float64     `json:"profit1" bson:"profit1"`
+	DisplaySymbol string      `json:"displaysymbol" bson:"displaysymbol"`
+	Symbol        string      `json:"symbol" bson:"symbol"`
+	StopLoss      float64     `json:"stoploss" bson:"stoploss"`
+	PatternUrl    string      `json:"url" bson:"url"`
+	TimeFrame     string      `json:"timeframe" bson:"timeframe"`
+	Status        string      `json:"status" bson:"status"`
+	Entry         interface{} `json:"entry" bson:"entry"`
+	PatternClass  string      `json:"patternclass" bsom:"patternclass"`
 }
 
 type AlpacaEntryOrder struct {
